2933b12a7e1acd10b793f373d7a2ac940296db13
Vladimir Ivanov <iv-developer@mail.ru>
2021-04-24 22:21:51 +0300

    Add task project

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+/* components */
+
+//=include ../../bower_components/jquery/dist/jquery.min.js
+//=include ../../bower_components/slick-carousel/slick/slick.min.js
+//=include ../../bower_components/jquery.maskedinput/dist/jquery.maskedinput.js
+//=include ../../bower_components/select2/dist/js/select2.js
+
+//=include ./partials/app.js
